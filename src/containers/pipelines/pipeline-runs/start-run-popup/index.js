8fe6fe6f4aafc2442e26144d1013c6ba8fa6119e
Thistleman <codes5096@gmail.com>
2022-03-21 15:18:37 -0700

    Fixed typo

--- src/containers/pipelines/pipeline-runs/start-run-popup/index.js
+++ src/containers/pipelines/pipeline-runs/start-run-popup/index.js
@@ -252,7 +252,7 @@ const StartRunPopup = ({
           textcolor="lightBlue"
           onClick={handleOpenPiplineClick}
         >
-          Pipline Description
+          Pipeline Description
         </StyledButton>,
       ]}
       onOk={handleOk}
