1ff6857355373d152c1093f35ea465fdeacc8085
Thistleman <codes5096@gmail.com>
2022-03-23 15:10:07 -0700

    Might be close to finding the safari bug?

--- src/containers/pipelines/pipeline-dropdown/pipeline-selector.js
+++ src/containers/pipelines/pipeline-dropdown/pipeline-selector.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const PipelineSelector = (props) => {
 // assign required values to each element for click handler
+  console.log('IS THIS IS?!?', props);
   const { pipeline, updateFromDropdown } = props;
   return (
     <option
@@ -16,6 +18,17 @@ const PipelineSelector = (props) => {
       {pipeline.full_name}
     </option>
   );
+};
+
+PipelineSelector.propTypes = {
+  pipeline: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    full_name: PropTypes.string.isRequired,
+  }).isRequired,
+  updateFromDropdown: PropTypes.func.isRequired,
+
 };
 
 export default PipelineSelector;
